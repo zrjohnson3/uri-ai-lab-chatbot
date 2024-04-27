551a3398791381c562a751259edaec48aca987d7
zrjohnson3 <zrjohnson3@gmail.com>
2024-04-27 10:51:07 -0400

    Chatbot functionality into it's own component for modularity

--- App.tsx
+++ App.tsx
@@ -1,9 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { fetchAIResponse } from './api/ai';
-import tw from 'tailwind-react-native-classnames';
-import ChatBubble from './components/ChatBubble';
+import React from 'react';
 import Chatbot from './components/Chatbot';
 
 export default function App() {
